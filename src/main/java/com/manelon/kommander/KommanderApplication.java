ed39470b760fc3ea81aa9d1fff5b8e3b8e65aacc
manel <Manelon@users.noreply.github.com>
2023-02-21 23:40:16 +0000

    first commit

---
+++ src/main/java/com/manelon/kommander/KommanderApplication.java
@@ -0,0 +1,13 @@
+package com.manelon.kommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KommanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manelon/kommander/KommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manelon.kommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
